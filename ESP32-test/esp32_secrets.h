5a04d9e840730abd4621042c1b96b0d9e29cbc42
fix <freddy.ix.andrade@gmail.com>
2022-04-26 16:49:04 -0500

    proyecto co2

---
+++ ESP32-test/esp32_secrets.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID  "Totalplay-53A0"
+#define SECRET_PASS  "53A037B1Vr8Pa7HK"
+#define BROKER_USERNAME  "upy"
+#define BROKER_PASSWORD  "abcd1234"
+#define BROKER_IP  "192.168.100.105"
+#define CLIENT_NAME  "ESP32_client_1"
+#define BROKER_PORT  1883
